c0711a8427364e8e3e32452f2ecf6409ab34159b
stephenmark0 <133763929+stephenmark0@users.noreply.github.com>
2023-06-06 12:34:12 +0500

    Design fix

--- src/_screen/orders/Orders.jsx
+++ src/_screen/orders/Orders.jsx
@@ -1,14 +1,30 @@
 import Style from './style.module.css'
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+// import { orderData } from './orderData';
 // import { OrdersList } from './Orderlist';
 
 export { Orders };
 
 function Orders() {
     const mobile = useSelector(x => x.mobile.value)
-    const handleSubmit = () => {
-        // console.log("submit");
-    }
+
+    const [filter, setFilter] = useState({
+        orderNo: '',
+        status: '',
+        store: '',
+        merchant: '',
+        createdAt: '',
+    });
+    const handleChange = (e) => {
+        setFilter({ ...filter, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle filter logic here
+        console.log(filter);
+    };
+
     return (
         <div className={Style.order_mainContainer} style={{ marginLeft: mobile ? mobile.class : "290px" }}>
             <div className={Style.filters_wrapper}>
@@ -16,29 +32,30 @@ function Orders() {
                 <hr style={{ borderColor: "#dcdcdc", opacity: .3 }} />
 
                 <div className={Style.search_form}>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className={Style.filter_inputWrapper}>
                             <label className={Style.filter_label}>Search</label>
-                            <input name="search" className={Style.filter_input} type='text' placeholder='Order No, Store, Merchant, etc.' />
+                            <input onChange={handleChange} value={filter.name}
+                                name="orderNo" className={Style.filter_input} type='text' placeholder='Order No, Store, Merchant, etc.' />
                         </div>
                         <div className={Style.filter_inputWrapper}>
                             <label className={Style.filter_label}>Status</label>
-                            <input name="status" className={Style.filter_input} type='text' placeholder='Order Status' />
+                            <input onChange={handleChange} name="status" className={Style.filter_input} type='text' placeholder='Order Status' />
                         </div>
                         <div className={Style.filter_inputWrapper}>
                             <label className={Style.filter_label}>Store</label>
-                            <input name="store" className={Style.filter_input} type='text' placeholder='Search Stores' />
+                            <input onChange={handleChange} name="store" className={Style.filter_input} type='text' placeholder='Search Stores' />
                         </div>
                         <div className={Style.filter_inputWrapper}>
                             <label className={Style.filter_label}>Search Users</label>
-                            <input name="user" className={Style.filter_input} type='text' placeholder='Order No, Store, Merchant, etc.' />
+                            <input onChange={handleChange} name="merchant" className={Style.filter_input} type='text' placeholder='Order No, Store, Merchant, etc.' />
                         </div>
                         <div className={Style.filter_inputWrapper}>
                             <label className={Style.filter_label}>Created At</label>
-                            <input name="date" className={Style.filter_input} type='date' placeholder='Order No, Store, Merchant, etc.' />
+                            <input onChange={handleChange} name="createdAt" className={Style.filter_input} type='date' placeholder='Order No, Store, Merchant, etc.' />
                         </div>
                         <div className={Style.filter_buttonwrapper}>
-                            <button className={Style.filter_button} onClick={handleSubmit()}>Filter</button>
+                            <button type="submit" className={Style.filter_button} >Filter</button>
                         </div>
                     </form>
                 </div>
