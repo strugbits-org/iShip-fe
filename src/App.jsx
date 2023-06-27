887b847e6db502f5732c1409c15d2e1e1e417d4c
stephenmark0 <133763929+stephenmark0@users.noreply.github.com>
2023-06-27 19:16:33 +0500

    Routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
 import { history } from '_helpers';
 import { Nav, Alert, PrivateRoute, Sidebar } from '_components';
-import { Orders } from '_screen/orders';
+import { OrderLayout } from '_screen/orders';
 import { AccountLayout } from '_screen/account';
 import { UsersLayout } from '_screen/merchants';
+import { LocationLayout } from '_screen/location'
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 export { App };
 
 function App() {
     // init custom history object to allow navigation from 
     // anywhere in the react app (inside or outside components)
+    //CHECK IF ADMIN
+    const auth = useSelector(x => x.auth.value);
+    const [isAdmin, setIsAdmin] = useState(false);
+    useEffect(() => {
+        const role = auth?.user?.email
+        role === "stephen.mk091@gmail.com" ? setIsAdmin(true) : setIsAdmin(false)
+    }, [auth])
+
+
+
     history.navigate = useNavigate();
     history.location = useLocation();
-   
+
     return (
         <div className='main_container flex'>
             <Alert />
@@ -22,10 +35,21 @@ function App() {
             <div className='main_content'>
                 <Routes>
                     {/* private */}
-                    <Route element={<PrivateRoute />}>
-                        <Route path="/" element={<Orders />} />
-                        <Route path="users/*" element={<UsersLayout />} />
-                    </Route>
+                    {isAdmin ?
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/*" element={<OrderLayout />} />
+                            <Route path="users/*" element={<UsersLayout />} />
+                            <Route path="location/*" element={<LocationLayout />} />
+                        </Route>
+                        :
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/*" element={<OrderLayout />} />
+                            <Route path="users/*" element={<Navigate to="/" />} />
+                            <Route path="location/*" element={<LocationLayout />} />
+                            <Route path="*" element={<Navigate to="/" />} />
+                        </Route>
+                    }
+
                     {/* public */}
                     <Route path="account/*" element={<AccountLayout />} />
                     <Route path="*" element={<Navigate to="/" />} />
