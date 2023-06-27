6758904495a782feb95c7d441b00acf049a84501
stephenmark0 <133763929+stephenmark0@users.noreply.github.com>
2023-06-27 19:17:10 +0500

    Dummy data

---
+++ src/_helpers/dummy.jsx
@@ -0,0 +1,162 @@
+export const orderData = [
+    {
+        id: 1,
+        orderNo: 533014,
+        status: "On the way",
+        amount: "$28.33",
+        store: "Vicenta Landing",
+        merchant: "Audrey Weimann",
+        products: "3 items",
+        createdAt: "May 13, 2023 1:38 AM"
+    },
+    {
+        id: 2,
+        orderNo: 326595,
+        status: "Cancelled",
+        amount: "$37.01",
+        store: "Laverna Haven",
+        merchant: "Lizeth Haley",
+        products: "3 items",
+        createdAt: "June 6, 2023 2:00 AM"
+    },
+    {
+        id: 3,
+        orderNo: 635985,
+        status: "Delivered",
+        amount: "$55.30",
+        store: "Tillman Grove",
+        merchant: "Delfina Littel",
+        products: "4 items",
+        createdAt: "May 8, 2023 4:22 PM"
+    },
+    {
+        id: 4,
+        orderNo: 580241,
+        status: "Pending",
+        amount: "$25.91",
+        store: "Pearline Inlet",
+        merchant: "Jess Douglas",
+        products: "3 items",
+        createdAt: "May 9, 2023 4:19 PM"
+    },
+    {
+        id: 5,
+        orderNo: 693596,
+        status: "On the way",
+        amount: "$8.06",
+        store: "Luettgen Greens",
+        merchant: "Wyman Koelpin",
+        products: "1 items",
+        createdAt: "May 10, 2023 9:38 PM"
+    },
+    {
+        id: 6,
+        orderNo: 623740,
+        status: "On the way",
+        amount: "$23.80",
+        store: "Pearline Inlet",
+        merchant: "Felicita Krajcik",
+        products: "3 items",
+        createdAt: "May 18, 2023 4:30 PM"
+    },
+    {
+        id: 7,
+        orderNo: 885503,
+        status: "Delivered",
+        amount: "$19.31",
+        store: "Garret Route",
+        merchant: "Shaylee Murray",
+        products: "2 items",
+        createdAt: "May 10, 2023 5:04 PM"
+    },
+    {
+        id: 8,
+        orderNo: 378003,
+        status: "Pending",
+        amount: "$45.33",
+        store: "Maddison Vista",
+        merchant: "Paul Wyman",
+        products: "4 items",
+        createdAt: "May 15, 2023 9:52 PM"
+    },
+    {
+        id: 9,
+        orderNo: 319108,
+        status: "On the way",
+        amount: "$45.11",
+        store: "Emil Crossing",
+        merchant: "Eryn Haag",
+        products: "3 items",
+        createdAt: "May 21, 2023 8:09 AM"
+    },
+    {
+        id: 10,
+        orderNo: 376198 ,
+        status: "Cancelled",
+        amount: "$5.79",
+        store: "Keara Ville",
+        merchant: "Elise Rosenbaum",
+        products: "1 items",
+        createdAt: "May 16, 2023 8:44 AM"
+    },
+
+];
+
+export const locationData = [
+    {
+        id: "01",
+        phoneNo: "1234567890",
+        address: "7855, Finch Avenue West, Brampton, Ontario, Canda",
+        zipcode: "M2K 1W9",
+        status: "Active"
+    },
+    {
+        id: "02",
+        phoneNo: "1234567890",
+        address: "5601, Steeles Avenue West, Toronto, Canada",
+        zipcode: "M3K 0C3",
+        status: "Inactive"
+    },
+    {
+        id: "03",
+        phoneNo: "1234567890",
+        address: "290, Derry Road West, Derry West, Ontario, Canada",
+        zipcode: "M6J 2K8",
+        status: "Active"
+    }, 
+    {
+        id: "04",
+        phoneNo: "1234567890",
+        address: "7855, Finch Avenue West, Brampton, Ontario, Canda",
+        zipcode: "M2K 1W9",
+        status: "Active"
+    },
+    {
+        id: "05",
+        phoneNo: "1234567890",
+        address: "5601, Steeles Avenue West, Toronto, Canada",
+        zipcode: "M3K 0C3",
+        status: "Inactive"
+    },
+    {
+        id: "06",
+        phoneNo: "1234567890",
+        address: "290, Derry Road West, Derry West, Ontario, Canada",
+        zipcode: "M6J 2K8",
+        status: "Active"
+    }, 
+    {
+        id: "07",
+        phoneNo: "1234567890",
+        address: "7855, Finch Avenue West, Brampton, Ontario, Canda",
+        zipcode: "M2K 1W9",
+        status: "Active"
+    },
+    {
+        id: "08",
+        phoneNo: "1234567890",
+        address: "5601, Steeles Avenue West, Toronto, Canada",
+        zipcode: "M3K 0C3",
+        status: "Inactive"
+    },
+];
